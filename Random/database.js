f02f8364ad06768f423a0eb37b891d71357721fb
M4RCU5BUBR35KO <113419641+M4RCU5BUBR35KO@users.noreply.github.com>
2023-05-11 13:28:56 +0200

    Add files via upload

---
+++ Random/database.js
@@ -0,0 +1,8 @@
+let maler = {
+    "kortArray": [
+        { "navn": "Jonas", "skole": "Drammen Videregående Skole", "adresse": "adr: Drammenveien 37", "tlf": "tlf: Android","sitat1": "Je liker å regge rundt med damer", "sitat2": "Raggen går", "sitat3": "Hei" },
+        { "navn": "Gunnar", "skole": "Nesbru Videregående Skole", "adresse": "adr: Goat 69", "tlf": "tlf: Iphone 17", "sitat1": "Jeg har Vulvaen", "sitat2": "Dama di er stygg", "sitat3": "Bygg og anlegg kriger" },
+        { "navn": "Gunnleif", "skole": "Røyken Videregående Skole", "adresse": "adr: Sigggata 102", "tlf": "tlf: Nokia", "sitat1": "jeg sigger som faen", "sitat2": "Anders stinker balle", "sitat3": "raggen går" },
+        { "navn": "Stein", "skole": "Gol Videregående Skole", "adresse": "adr: Golholsol gata 23", "tlf": "tlf: Samsung", "sitat1": "Raggen går snuppa", "sitat2": "Je snakker med feit L", "sitat3": "Bygg og anlegg" }
+    ]
+}
